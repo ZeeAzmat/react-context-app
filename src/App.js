d931167ae60445d8b1f75703bf01406f82ad111b
Zeeshan Azmat <zeeshanazmat56@gmail.com>
2020-06-17 13:19:42 +0500

    multiple consumer contexts

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 import ThemeContextProvider from './contexts/ThemeContext';
 
 const App = () => {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
