848fe3b6e4d95200f0d902521ace9a2da2fb2888
Zeeshan Azmat <zeeshanazmat56@gmail.com>
2020-06-17 12:55:05 +0500

    context consumer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,23 @@ class Navbar extends Component {
   static contextType = ThemeContext;
 
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+
+        return (
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        );
+      }}
+      </ThemeContext.Consumer>
     );
   }
 }
